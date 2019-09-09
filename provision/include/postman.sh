b7a0693e7c5ddcdcb786a5f4b907f6744322e24e
Naoaki Obiki <obiki.wk@gmail.com>
2019-09-09 14:21:02 +0900

    :bug: fix postman symlink permission denied

--- provision/include/postman.sh
+++ provision/include/postman.sh
@@ -1,4 +1,4 @@
 sudo wget https://dl.pstmn.io/download/latest/linux64 -O /usr/local/lib/postman.tar.gz
 sudo tar zxf /usr/local/lib/postman.tar.gz -C /usr/local/lib/
-ln -s /usr/local/lib/Postman/Postman ~/bin/postman
+sudo ln -s /usr/local/lib/Postman/Postman /usr/local/bin/postman
 
