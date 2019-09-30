a3866cc275602462d6bcbcb5a0cbecbf9886afe6
Naoaki Obiki <obiki.wk@gmail.com>
2019-09-30 14:36:11 +0900

    fix enhancd permission denied

--- provision/include/enhancd.sh
+++ provision/include/enhancd.sh
@@ -1,2 +1,4 @@
-sudo git clone "https://github.com/b4b4r07/enhancd.git" /usr/local/lib/enhancd && sudo chmod +x /usr/local/lib/enhancd/init.sh
+sudo git clone "https://github.com/b4b4r07/enhancd.git" /usr/local/lib/enhancd
+sudo chmod +x /usr/local/lib/enhancd/init.sh
+sudo chown -R 1000:1000 /usr/local/lib/enhancd
 echo 'source /usr/local/lib/enhancd/init.sh' >> /tmp/.include_profile
